68634e8f1dbdf5154dbdd00a35ed181ac6dd668f
Rodrigo Torres <rtmr@cesar.school>
2025-05-07 20:05:55 -0300

    para responder a questa 4

--- mergeSort.c
+++ mergeSort.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
 void merge(int arr[], int esquerda, int meio, int direita) {
     int i, j, k;
@@ -51,30 +54,57 @@ void mergeSort(int arr[], int esquerda, int direita) {
     }
 }
 
-void imprimirArray(int arr[], int tamanho) {
-    for (int i = 0; i < tamanho; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
+void gerarArrayAleatorio(int arr[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        arr[i] = rand() % 100000; // números entre 0 e 99.999
+    }
+}
+
+double calcularMedia(double tempos[], int n) {
+    double soma = 0.0;
+    for (int i = 0; i < n; i++) {
+        soma += tempos[i];
+    }
+    return soma / n;
+}
+
+double calcularDesvioPadrao(double tempos[], int n, double media) {
+    double soma = 0.0;
+    for (int i = 0; i < n; i++) {
+        soma += (tempos[i] - media) * (tempos[i] - media);
+    }
+    return sqrt(soma / n);
 }
 
 int main() {
-    int n;
-    printf("Digite o tamanho do array: ");
-    scanf("%d", &n);
+    int tamanhos[] = {100, 1000, 10000}; // tamanhos pequenos, médios e grandes
+    int num_execucoes = 30;
+    srand(time(NULL));
 
-    int arr[n];
-    printf("Digite os %d elementos do array:\n", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
+    for (int t = 0; t < 3; t++) {
+        int n = tamanhos[t];
+        double tempos[num_execucoes];
+
+        printf("\n--- Tamanho do array: %d ---\n", n);
+
+        for (int exec = 0; exec < num_execucoes; exec++) {
+            int arr[n];
+            gerarArrayAleatorio(arr, n);
+
+            clock_t inicio = clock();
+            mergeSort(arr, 0, n - 1);
+            clock_t fim = clock();
+
+            tempos[exec] = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+        }
+
+        double media = calcularMedia(tempos, num_execucoes);
+        double desvio = calcularDesvioPadrao(tempos, num_execucoes, media);
+
+        printf("Execuções: %d\n", num_execucoes);
+        printf("Tempo médio: %.6f segundos\n", media);
+        printf("Desvio padrão: %.6f segundos\n", desvio);
     }
-
-    printf("Array original:\n");
-    imprimirArray(arr, n);
-
-    mergeSort(arr, 0, n - 1);
-
-    printf("Array ordenado com Merge Sort:\n");
-    imprimirArray(arr, n);
 
     return 0;
 }
